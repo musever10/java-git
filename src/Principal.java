36cd48c42620551643a612774249a64ecbbf2c69
Ever Rangel <musever10@gmail.com>
2019-07-15 00:09:44 -0500

    cambié mensajes en Eclipse

--- src/Principal.java
+++ src/Principal.java
@@ -4,10 +4,10 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo desde Java");
-		System.out.println("Modificaciï¿½n de cï¿½digo");
+		System.out.println("Modificación de código");
 		System.out.println("Mensaje para repositorio en la nube");
 		System.out.println("Mensaje al cargar en eclipse");
-                System.out.println("Mensaje al cargar en Netbeans");
+        System.out.println("Mensaje al cargar en Netbeans");
 	}
 
 }
