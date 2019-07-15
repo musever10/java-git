a5d9677d0e596ee3f2409e653405fe71e282c535
Ever Rangel <musever10@gmail.com>
2019-07-14 21:45:30 -0500

    Modificación de mensajes

--- src/Principal.java
+++ src/Principal.java
@@ -4,6 +4,7 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo desde Java");
+		System.out.println("Modificación de código");
 	}
 
 }
